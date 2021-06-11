2053e5ffc0516a2c59ca0f1cb04d969e395c676a
Neil <neil.roy63@gmail.com>
2021-06-11 13:01:04 +1000

    fix/updated index js and lib files

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const inquirer = require("inquirer");
+// to save files
+const fs = require("fs");
+
+const Employee = require("./lib/Employee");
+
+const Manager = require("./lib/Manager");
+
+// what happens when you start inquirer, gets data, uses it to put it into the classess, source folder will contain helper files where things get generated, will be a way to save it in the dist folder....
